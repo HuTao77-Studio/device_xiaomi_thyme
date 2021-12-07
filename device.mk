c5fb46e3d082d77676dd0242f1c6309e99c793cd
sbwml <admin@cooluc.com>
2021-12-08 05:03:24 +0530

    thyme: Build vendor.display.config 1.9 * Some blobs does not allow the use of vendor.display.config 2.0   vendor.display.config 1.0 is required. Hence Build vendor.display.config 1.9
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -230,6 +230,8 @@ PRODUCT_PACKAGES += \
     memtrack.kona \
     gralloc.kona \
     vendor.display.config@1.5 \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
