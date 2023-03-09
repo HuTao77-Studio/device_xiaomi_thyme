d8575a9adecc9fc5b50d5ef48b510c2677d13cca
LynnrinChan <lynnrin@lynnrin.moe>
2023-03-09 11:13:18 +0800

    thyme: Use ih8sn for SN

--- device.mk
+++ device.mk
@@ -301,6 +301,18 @@ PRODUCT_PACKAGES += \
     init.xiaomi.rc \
     ueventd.qcom.rc
 
+# Ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ih8sn/ih8sn.conf:/system/etc/ih8sn.conf
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/ih8sn \
+    system/etc/ih8sn.conf \
+    system/etc/init/ih8sn.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/kona-mtp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/kona-mtp-snd-card_Button_Jack.kl
