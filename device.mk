339c53f35720768716c9ee63a85958fba938b3ed
Mayur <ultramayur123@gmail.com>
2023-10-30 17:39:06 +0100

    thyme: rename libstdc++.vendor to libstdc++_vendor found following in splash logcat and it's also renamed in source Abort message: 'duplicate: libstdc++.so is provided by default and system in [vendor]'

--- device.mk
+++ device.mk
@@ -207,7 +207,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor \
-    libstdc++.vendor \
+    libstdc++_vendor \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
