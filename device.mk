7fcfaf739ba7389302dc5c58f0df5a28863678a2
Fuutao <fuutaoao@gmail.com>
2024-04-21 14:41:37 +0800

    Revert "thyme: Build common touch hal"
    This reverts commit c22fa6178040c49c09d18ce6587e684cf04adfdc.
    
    Reason: Kernel part is not implemented.

--- device.mk
+++ device.mk
@@ -499,10 +499,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi
-
 # Touchfeature
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
