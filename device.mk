519632b9b9d9878f27551d2722d7bb13c35a9157
Fuutao <fuutaoao@gmail.com>
2024-05-27 19:11:00 +0800

    thyme: overlay: WifiRes remove common flag
     * WifiResCommon -> WifiResThyme
    
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- device.mk
+++ device.mk
@@ -543,7 +543,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiResCommon \
+    WifiResThyme \
     wpa_supplicant \
     wpa_supplicant.conf
 
