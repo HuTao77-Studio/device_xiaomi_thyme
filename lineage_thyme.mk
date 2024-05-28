adcb55a37742510f50fb411b5d86593afe5a5fff
Fuutao <fuutaoao@gmail.com>
2024-05-28 22:59:52 +0800

    thyme: Enable GMS support
    Signed-off-by: Fuutao <fuutaoao@gmail.com>

--- lineage_thyme.mk
+++ lineage_thyme.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/thyme/device.mk)
 # Inherit some common Crdroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device flags
+WITH_GMS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_thyme
 PRODUCT_DEVICE := thyme
